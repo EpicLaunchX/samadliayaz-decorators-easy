35ed9e6df8e4eed6447e87ffa7b7eaa9e5211443
Ayaz Samadli <samadlia@ayazs-air.speedport.ip>
2024-05-19 23:10:09 +0200

    feat: buy_ticket_for_teens

--- src/pytemplate/__init__.py
+++ src/pytemplate/__init__.py
@@ -1,5 +1,5 @@
 from pytemplate.domain.models import Movie, movie_factory  # noqa
-from pytemplate.service.tickets import buy_ticket_for_children  # noqa
-from pytemplate.utils.decorator import age_limit_6plus  # noqa
+from pytemplate.service.tickets import buy_ticket_for_children, buy_ticket_for_teens  # noqa
+from pytemplate.utils.decorator import age_limit_6plus, age_limit_13plus  # noqa
 
 __version__ = "0.0.1"
