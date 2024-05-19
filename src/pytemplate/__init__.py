54e75ee3ffcbc5605d7bd3786fe1fbb0a875ea72
Ayaz Samadli <samadlia@ayazs-air.speedport.ip>
2024-05-20 01:15:16 +0200

    feat: add main

--- src/pytemplate/__init__.py
+++ src/pytemplate/__init__.py
@@ -1,4 +1,5 @@
 from pytemplate.domain.models import Movie, movie_factory  # noqa
+from pytemplate.entrypoints.cli.main import main  # noqa
 from pytemplate.service.tickets import buy_ticket_for_children, buy_ticket_for_teens  # noqa
 from pytemplate.utils.decorator import age_limit_6plus, age_limit_13plus  # noqa
 
---
+++ tests/integration_test.py
@@ -0,0 +1,45 @@
+from unittest.mock import patch
+
+import pytest
+
+from pytemplate import main
+
+
+def test_buy_ticket_for_children(capsys):
+    inputs = ["Frozen", "8", "6"]
+    with patch("builtins.input", side_effect=inputs):
+        main()
+    captured = capsys.readouterr()
+    assert captured.out == "You are allowed to watch Frozen.\n"
+
+
+def test_buy_ticket_for_teens(capsys):
+    inputs = ["Batman", "16", "13"]
+    with patch("builtins.input", side_effect=inputs):
+        main()
+    captured = capsys.readouterr()
+    assert captured.out == "You are allowed to watch Batman.\n"
+
+
+def test_age_limit(capsys):
+    inputs = ["Blabla", "20", "10"]
+    with patch("builtins.input", side_effect=inputs):
+        main()
+    captured = capsys.readouterr()
+    assert captured.out == "We don't support this age limit.\n"
+
+
+def test_children_not_allowed(capsys):
+    inputs = ["Frozen", "5", "6"]
+    with patch("builtins.input", side_effect=inputs):
+        main()
+    captured = capsys.readouterr()
+    assert captured.out == "Sorry, you are not old enough to watch Frozen!\n"
+
+
+def test_teens_not_allowed(capsys):
+    inputs = ["Batman", "12", "13"]
+    with patch("builtins.input", side_effect=inputs):
+        main()
+    captured = capsys.readouterr()
+    assert captured.out == "Sorry, you are not old enough to watch Batman!\n"
