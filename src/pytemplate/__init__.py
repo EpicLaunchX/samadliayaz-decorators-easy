a259569c18f16158eb740c1c374d75b80c3525f4
Ayaz Samadli <samadlia@ayazs-air.speedport.ip>
2024-05-19 22:43:51 +0200

    feat: add decorator

--- src/pytemplate/__init__.py
+++ src/pytemplate/__init__.py
@@ -1,3 +1,5 @@
 from pytemplate.domain.models import Movie, movie_factory  # noqa
+from pytemplate.service.tickets import buy_ticket_for_children  # noqa
+from pytemplate.utils.decorator import age_limit_6plus  # noqa
 
 __version__ = "0.0.1"
---
+++ src/pytemplate/utils/decorator.py
@@ -0,0 +1,11 @@
+from typing import Callable
+
+from pytemplate.domain.models import Movie
+
+
+def age_limit_6plus(func: Callable[[Movie], str]) -> Callable[[Movie], str]:
+    def wrapper(movie: Movie):
+        if movie.customer_age < 6:
+            return f"Sorry, you are not old enough to watch {movie.name}!"
+        return func(movie)
+    return wrapper
